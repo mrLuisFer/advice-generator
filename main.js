8b3722ad20967df930fb8e4398f8a23e883bd696
mrLuisFer <mrluisfeer@gmail.com>
2022-04-10 22:46:44 -0500

    remove unnecessary code and remove loader state

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './src/css/style.css'
 import './src/css/loader.css'
 import getRandomAdvice from './src/scripts/getRandomAdvice'
-
 const adviceIcon = document.querySelector("#advice-icon")
 
 window.addEventListener('load', () => getRandomAdvice())
-adviceIcon.addEventListener('click', () => getRandomAdvice())
+adviceIcon.addEventListener('click', getRandomAdvice)
--- src/scripts/getRandomAdvice.js
+++ src/scripts/getRandomAdvice.js
@@ -1,24 +1,20 @@
+const adviceNumber = document.querySelector('#advice-number');
+const adviceContent = document.querySelector('#advice-content');
+const loaderDiv = document.querySelector('#loader');
 
-const adviceNumber = document.querySelector("#advice-number")
-const adviceContent = document.querySelector("#advice-content")
-const loaderDiv = document.querySelector("#loader")
+const url = 'https://api.adviceslip.com/advice';
 
-let loader = true
 const getRandomAdvice = async () => {
-  const url = "https://api.adviceslip.com/advice"
+  const res = await fetch(url);
+  const data = await res.json();
+  const advice = data.slip;
 
-  try {
-    const res = await fetch(url)
-    const data = await res.json()
-    const advice = data.slip
-    if (data) loader = false
-    if (!loader) {
-      loaderDiv.textContent = ""
-      loaderDiv.style.display = "none"
-      adviceContent.textContent = `"${advice.advice}"`
-      adviceNumber.textContent = `#${advice.id}`
-    }
-  } catch (err) { console.error(err) }
-}
+  if (data) {
+    loaderDiv.textContent = '';
+    loaderDiv.style.display = 'none';
+    adviceContent.textContent = `"${advice.advice}"`;
+    adviceNumber.textContent = `#${advice.id}`;
+  }
+};
 
-export default getRandomAdvice
+export default getRandomAdvice;
