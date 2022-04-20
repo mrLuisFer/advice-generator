da7733683d111d55f97e799ae07daaca388c0cfc
mrLuisFer <mrluisfeer@gmail.com>
2022-04-20 12:56:20 -0500

    add no select in the divider and add try-catch in getAdvice function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-import './src/css/style.css'
-import './src/css/loader.css'
-import getRandomAdvice from './src/scripts/getRandomAdvice'
-const adviceIcon = document.querySelector("#advice-icon")
+import './src/css/style.css';
+import './src/css/loader.css';
+import getRandomAdvice from './src/scripts/getRandomAdvice';
+const adviceIcon = document.querySelector('#advice-icon');
 
-window.addEventListener('load', () => getRandomAdvice())
-adviceIcon.addEventListener('click', getRandomAdvice)
+window.addEventListener('load', () => getRandomAdvice());
+adviceIcon.addEventListener('click', () => {
+  console.log('click');
+  getRandomAdvice();
+});
--- src/scripts/getRandomAdvice.js
+++ src/scripts/getRandomAdvice.js
@@ -3,17 +3,20 @@ const adviceContent = document.querySelector('#advice-content');
 const loaderDiv = document.querySelector('#loader');
 
 const url = 'https://api.adviceslip.com/advice';
-
 const getRandomAdvice = async () => {
-  const res = await fetch(url);
-  const data = await res.json();
-  const advice = data.slip;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const advice = data.slip;
 
-  if (data) {
-    loaderDiv.textContent = '';
-    loaderDiv.style.display = 'none';
-    adviceContent.textContent = `"${advice.advice}"`;
-    adviceNumber.textContent = `#${advice.id}`;
+    if (data) {
+      loaderDiv.textContent = '';
+      loaderDiv.style.display = 'none';
+      adviceContent.textContent = `"${advice.advice}"`;
+      adviceNumber.textContent = `#${advice.id}`;
+    }
+  } catch (err) {
+    console.error(err);
   }
 };
 
